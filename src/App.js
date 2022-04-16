9ebf0aa076e480afc560bea17565314f04616252
Berk Disli <berkdisli@yahoo.com>
2022-04-16 10:38:49 +0200

    initial state, favicon

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import Wrapper from './components/Wrapper';
+import React, { useState } from "react";
+import ButtonBox from './components/ButtonBox';
+import Screen from './components/Screen';
+
 
 const App = () => {
+
+  let [calc, setCalc] = useState({
+    sign: "",
+    num: 0,
+    res: 0,
+  });
+
   return (
-    <Wrapper />
+    <Wrapper>
+      <Screen value={calc.num ? calc.num : calc.res} />
+      <ButtonBox />
+    </Wrapper>
+
   );
 };
 export default App;
